1de6b0f9cd3a48f9a18be42a125588bdda4c21df
Chris Hung <yinye1986@gmail.com>
2024-12-17 12:42:19 +0800

    bak

--- OS/src/loader.asm
+++ OS/src/loader.asm
@@ -3,11 +3,50 @@
 dw 0x55aa; 魔数, 用于判断错误
 ; loader读到0x1000开头两个字节为55aa
 
+mov si, loading
+call print
+
 xchg bx, bx
 
-mov si, loading
+detect_memory:
+    ; 将 ebx 置为 0
+    xor ebx, ebx
 
-call print
+    ; es:di 结构体的缓存位置
+    mov ax, 0
+    mov es, ax
+    mov edi, ards_buffer
+
+    mov edx, 0x534d4150; 固定签名
+
+.next:
+    ; 子功能号
+    mov eax, 0xe820
+    ; ards 结构的大小 (字节)
+    mov ecx, 20
+    ; 调用 0x15 系统调用
+    int 0x15
+
+    ; 如果 CF 置位，表示出错
+    jc error
+
+    ; 将缓存指针指向下一个结构体
+    add di, cx
+
+    ; 将结构体数量加一
+    inc dword [ards_count]
+
+    cmp ebx, 0
+    jnz .next
+
+    mov si, detecting
+    call print
+
+    ; xchg bx, bx
+
+    ; mov byte [0xb8000], 'P'
+
+    jmp prepare_protected_mode
 
 jmp $
 
@@ -25,3 +64,16 @@ print:
 
 loading:
     db "Loading Onix...", 10, 13, 0; \n\r
+detecting:
+    db "Detecting memory success...", 10, 13, 0
+
+error:
+    mov si, .msg
+    call print
+    hlt; 让 CPU 停止
+    jmp $
+    .msg db "Loading Error!!!", 10, 13, 0
+
+ards_count:
+    dw 0
+ards_buffer:
