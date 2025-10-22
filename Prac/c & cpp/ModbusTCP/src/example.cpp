6272ffe612bb1b8b796e38c8786cfac1d24983dd
shins0u <yinye1986@gmail.com>
2025-10-22 16:07:15 +0800

    Sync

--- Prac/c & cpp/ModbusTCP/src/example.cpp
+++ Prac/c & cpp/ModbusTCP/src/example.cpp
@@ -4,7 +4,7 @@ using namespace hcz::modbusTCP;
 int main() {
   // 02
   ModbusTCPFrame a =
-      CreateFrame::createReadInputStatus(1, 1, 8); // 事物標識符，起始地址，個數
+      CreateFrame::createReadInputStatus(1, 1, 8); // 事务標識符，起始地址，個數
   ModbusTCPFrame b =
       CreateFrame::createWriteSingleCoil(2, 1, 1); // 事務標識符，地址，值(bool)
   ModbusTCPFrame c = CreateFrame::createWriteCoil(3, 1, 9, {0x20, 0x01});
